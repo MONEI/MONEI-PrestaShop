95df42988809d6b4835084bcc51bccb43359a6f1
Dmitriy <dn@monei.com>
2024-02-09 18:26:57 +0100

    fix controllers

--- controllers/admin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class MoneiPaymentsSettingsController extends AdminController
-{
-    function __construct()
-    {
-        Tools::redirectAdmin('index.php?controller=AdminModules&configure=moneipayments&token=' . Tools::getAdminTokenLite('AdminModules'));
-    }
-}
