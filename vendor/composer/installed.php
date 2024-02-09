e036bbd25f9a830b7144ebe8e8da71d4de68399f
Dmitriy <dn@monei.com>
2024-02-09 15:10:21 +0100

    refactor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'monei/monei-presta-shop',
+        'pretty_version' => 'dev-next',
+        'version' => 'dev-next',
+        'reference' => '9b4540a9e2995ee5a3129464f59c07b8a788e6ad',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'foroco/php-browser-detection' => array(
+            'pretty_version' => 'v2.3',
+            'version' => '2.3.0.0',
+            'reference' => '8c1714543b85f56d6eec19bb8263deb80922318d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../foroco/php-browser-detection',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monei/monei-presta-shop' => array(
+            'pretty_version' => 'dev-next',
+            'version' => 'dev-next',
+            'reference' => '9b4540a9e2995ee5a3129464f59c07b8a788e6ad',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
