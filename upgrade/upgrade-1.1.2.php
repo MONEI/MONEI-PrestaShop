1464ff1d494431d5cdf112240cb9e0c1d3055072
Jehyson Bustamante <gerencia@presteamshop.com>
2024-04-03 14:42:27 -0500

    Revisión: 5 - Traducción y cambio de version 1.1.2

--- upgrade/upgrade-1.1.2.php
+++ upgrade/upgrade-1.1.2.php
@@ -3,7 +3,7 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-function upgrade_module_1_0_2($module)
+function upgrade_module_1_1_2($module)
 {
     Configuration::updateValue('MONEI_ACCOUNT_ID', '');
 
