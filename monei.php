52330d2f1bc60ca0be5efc899938da434f7abb80
Dmitriy <dn@monei.com>
2024-11-27 21:37:49 +0100

    chore: release v1.4.12

--- monei.php
+++ monei.php
@@ -44,7 +44,7 @@ class Monei extends PaymentModule
         $this->displayName = 'MONEI Payments';
         $this->name = 'monei';
         $this->tab = 'payments_gateways';
-        $this->version = '1.4.11';
+        $this->version = '1.4.12';
         $this->author = 'MONEI';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = ['min' => '1.7', 'max' => _PS_VERSION_];
