9fc6e86e9727b566ce72251f6f76ed260e543782
Jehyson Bustamante <gerencia@presteamshop.com>
2025-02-22 11:05:07 -0500

    Update upgrade-1.5.2.php

--- upgrade/upgrade-1.5.2.php
+++ upgrade/upgrade-1.5.2.php
@@ -5,11 +5,11 @@ if (!defined('_PS_VERSION_')) {
 
 function upgrade_module_1_5_2()
 {
-    $sql = 'ALTER TABLE `' . _DB_PREFIX_ . 'monei` DROP `locked_at`;';
-    Db::getInstance()->execute($sql);
+    // $sql = 'ALTER TABLE `' . _DB_PREFIX_ . 'monei` DROP `locked_at`;';
+    // Db::getInstance()->execute($sql);
 
-    $sql = 'ALTER TABLE `' . _DB_PREFIX_ . 'monei` DROP `locked`;';
-    Db::getInstance()->execute($sql);
+    // $sql = 'ALTER TABLE `' . _DB_PREFIX_ . 'monei` DROP `locked`;';
+    // Db::getInstance()->execute($sql);
 
     return true;
 }
